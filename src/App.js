945669b18c9929e5aa32be3021f5b90dca23a203
HannesVer <hv@Hanness-MacBook-Pro.local>
2023-06-07 12:14:53 +0200

    first

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Tile from './Tile';
 import './all.css';
 
 function Home() {
-  const locations = ['Cambridge','Oxford','Camden', 'Hackney', 'Shoreditch', 'Fulham', 'Notting Hill', 'Chelsea', 'Tottenham', 'Sainsbury', 'Wembley', 'Mayfair', 'Hoxton'];
+  const locations = ['Camden', 'Hackney', 'Shoreditch', 'Fulham', 'Notting Hill'];
   const imageNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // Assuming there are 9 images from A.png to I.png
   const tileNames = ['Buzzing', 'Glorious', 'Curious', 'Sturdy', 'Cosmic', 'Magic', 'Flaming', 'Cuddly', 'Bumbling']; // Creative names for buses
 
   const initialCaptions = Array(9).fill().map(() => randomLocation());
   const [captions, setCaptions] = useState(initialCaptions);
-  const [countdown, setCountdown] = useState(3);
+  const [countdown, setCountdown] = useState(10);
 
   function randomLocation() {
     const index = Math.floor(Math.random() * locations.length);
@@ -19,7 +19,7 @@ function Home() {
   useEffect(() => {
     const timer = setInterval(() => {
       setCaptions(captions.map(() => randomLocation()));
-      setCountdown(3);
+      setCountdown(10);
     }, 10000);
 
     const countdownTimer = setInterval(() => {
@@ -48,7 +48,7 @@ function Home() {
     <div className='home'>
       <h1>Welcome to peekAbus!</h1>
       <div className='countdown'>
-        <p>Busses moving in: {countdown}s</p>
+        <p>Changing in: {countdown}s</p>
       </div>
       <div className='grid'>
         {tiles}
