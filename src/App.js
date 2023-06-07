6015c3a1d9b30a49bd78394d6d5176d07aab1eae
HannesVer <hv@Hanness-MacBook-Pro.local>
2023-06-07 12:12:29 +0200

    first

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Home() {
   useEffect(() => {
     const timer = setInterval(() => {
       setCaptions(captions.map(() => randomLocation()));
-      setCountdown(10);
+      setCountdown(5);
     }, 10000);
 
     const countdownTimer = setInterval(() => {
